4aa39b50f60e8b426e3c798060ac27673d2f202c
Harsha30ywffb7 <93146635+Harsha30ywffb7@users.noreply.github.com>
2024-06-02 17:37:20 +0530

    async promises

--- Interview/EventListeners.js
+++ Interview/EventListeners.js
@@ -1,3 +1,4 @@
+const colors = ["black", "blue", "pink", "orange", "green" , "red"]
 function attachEventListeners(){
     let count =0;
     document.getElementById('button').addEventListener("click", ()=>{
@@ -5,4 +6,9 @@ function attachEventListeners(){
 })
 }
 
-attachEventListeners();
+//attachEventListeners();
+
+document.getElementById('button').addEventListener('click',()=>{
+    document.body.style.backgroundColor= colors[Math.floor(Math.random()*6)];
+    console.log(Math.floor(Math.random()*6))
+})
--- Interview/async.js
+++ Interview/async.js
@@ -13,3 +13,59 @@ async function fetchdata(){
 
 const data = fetchdata();
 // const readData = data.then(res => console.log(`data from the async function${res}`));
+
+
+// ! check how async works here.
+
+const promise1 = new Promise(function (resolve,reject){
+    setInterval(function (){
+        resolve("promise 1 is resolved");
+    }, 40000); 
+});
+
+const promise2 = new Promise(function (resolve,reject){
+    setInterval(function (){
+        resolve("promise 2 is resolved");
+    }, 20000);
+});
+
+// without await
+async function getData1(){
+    // returns resolve of the promise.
+   return promise1;
+}
+
+//!const data1 = getData().then(res => console.log(res));
+
+// with await
+
+async function getData2(){
+    
+    const data1 = await promise1;
+    console.log(data1)
+    console.log("Harsha, How are you?")
+    // after 10sec only console prints if promise is only resolved then next line prints.
+
+
+    const data2 = await promise2;
+    console.log(data2)
+    console.log("Harsha, How are you * 2?")
+}
+
+//getData2();
+
+
+async function getData3(){
+    
+    const data1 = await promise2;
+    console.log(data1)
+    console.log("Harsha, How are you?")
+    // after 10sec only console prints if promise is only resolved then next line prints.
+
+
+    const data2 = await promise1;
+    console.log(data2)
+    console.log("Harsha, How are you * 2?")
+}
+
+getData3();
